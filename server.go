6856d660fac134fc9220284accdfc526698bedb5
Danqi Liao <danqiliao@gmail.com>
2016-09-24 01:09:46 -0500

    parse table row with intructor and data

--- server.go
+++ server.go
@@ -11,15 +11,34 @@ import (
 )
 
 type Course struct {
+	Number string
 	Title string
-	Start string
-	End string
-	Instructor string
-	Location string
+	Time []string
+	Instructor []string
 	Date []string
+	Seasons []string
+}
+
+func parseTimeAndInstructor(content string) (string, string){
+	var time, instructor = "", ""
+	if c := string(content[0]); c == "<" {
+		return time, instructor
+	}
+
+	for i, r := range content {
+		if c := string(r); c == "<" {
+			time = content[:i]
+		}
+		if c := string(r); c == ">" {
+			instructor = content[i+1:len(content)]
+		}
+	}
+
+	return time, instructor
 }
 
 func getDepartmentCourses(url string) {
+	// courses := make([]Course, 0)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("A wild error has occured while crawling url %s: %s", url, err)
@@ -39,18 +58,33 @@ func getDepartmentCourses(url string) {
 
 			//find table row
 			s1.Find("tr").Each(func (ii int, s2 *goquery.Selection) {
-				log.Printf(s2.Html())
-				log.Printf("\n")
+				var number, title = "", ""
 				s2.Find("td").Each(func (rowIndex int, s3 *goquery.Selection){
 					switch rowIndex {
 					case 0:
-						number := s3.Find("a").Text()
-						log.Printf(number)
+						number = s3.Find("a").Text()
 					case 1:
-						title := s3.Find("a").Text()
-						log.Printf(title)
+						title = s3.Find("a").Text()
+					case 2:
+						inner, _ := s3.Html()
+						time, instructor := parseTimeAndInstructor(inner)
+						log.Printf("Time: %s, Instructor: %s", time, instructor)
+					case 3:
+						// winter = s3.Html()
+						// log.Printf(s3.Html())
+						inner, _ := s3.Html()
+						time, instructor := parseTimeAndInstructor(inner)
+						log.Printf("Time: %s, Instructor: %s", time, instructor)
+					case 4:
+						// spring = s3.Html()
+						// log.Printf(s3.Html())
+						inner, _ := s3.Html()
+						time, instructor := parseTimeAndInstructor(inner)
+						log.Printf("Time: %s, Instructor: %s", time, instructor)
 					}
 				})
+				// log.Printf("%s %s %s %s %s\n",number, title, fall, winter, spring)
+				// course := Course{number, title, time, instructor, }
 			})
 		})
 	})
@@ -63,33 +97,6 @@ func getDepartmentCourses(url string) {
 	//log.Printf("resp: \n%v")
 	//log.Println(resp.Body)
 
-	// z := html.NewTokenizer(resp.Body)
-	// depth := 0
-	// for {
-	// 	tt := z.Next()
-	//
-	// 	switch tt {
-	// 	case html.ErrorToken:
-	// 		return
-	// 	case html.TextToken:
-	// 		if depth > 0 {
-	// 			log.Printf("TEXT: %v", z.Text())
-	// 		}
-	// 	case html.StartTagToken, html.EndTagToken:
-	// 		t := z.Token()
-	//
-	// 		isTable := t.Data == "table"
-	// 		if isTable {
-	// 			log.Printf("found the table: %s", t)
-	// 			log.Printf("value of table: %v%T", t, t)
-	// 		}
-	// 		if tt == html.StartTagToken {
-	// 			depth++
-	// 		} else {
-	// 			depth--
-	// 		}
-	// 	}
-	// }
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
